37da4bbfdfeab8c1584cbbe921dd8035dc83694d
Asantha <asantha@loops.lk>
2017-10-24 11:20:15 +0530

    all done

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	
-	"database": "mongodb://root:123@ds023523.mlab.com:23523/wedding_elegance",
+	"database": "mongodb://root:root123@ds159013.mlab.com:59013/wedding_elegance",
 	"port" : process.env.PORT||3000,
 	"secretKey" : "YourSecretKey"
 }
